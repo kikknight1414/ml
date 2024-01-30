494b94f8e33327aca3aa84f119e74fd056a9d3dd
kikknight1414 <141972923+kikknight1414@users.noreply.github.com>
2024-01-30 15:44:52 +0530

    Update backtrack.py

--- backtrack.py
+++ backtrack.py
@@ -1,25 +1,26 @@
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split 
-from sklearn.feature_extraction.text import CountVectorizer 
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
-msg = pd.read_csv('/content/engl.csv', names=['message', 'label']) 
-print("Total Instances of Dataset: ",msg.shape[0])
+
+msg = pd.read_csv('/content/engl.csv', names=['message', 'label'])
+print("Total Instances of Dataset: ", msg.shape[0])
 msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
-X = msg.message 
+X = msg.message
 y = msg.labelnum
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
+
 count_v = CountVectorizer()
-Xtrain_dm = count_v.fit_transform(Xtrain) 
+Xtrain_dm = count_v.fit_transform(Xtrain)
 Xtest_dm = count_v.transform(Xtest)
-df = pd.DataFrame(Xtrain_dm.toarray(),columns=count_v.get_feature_names_out())
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), 
-random_state=1) 
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit(Xtrain_dm, ytrain)
+
 pred = clf.predict(Xtest_dm)
 print(pred)
-print('Accuracy: ', accuracy_score(ytest, pred)) 
-print('Recall: ', recall_score(ytest, pred)) 
+print('Accuracy: ', accuracy_score(ytest, pred))
+print('Recall: ', recall_score(ytest, pred))
 print('Precision: ', precision_score(ytest, pred))
 print('Confusion Matrix: \n', confusion_matrix(ytest, pred))
